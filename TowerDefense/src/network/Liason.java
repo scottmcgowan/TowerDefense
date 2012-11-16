156526ba4f7152df7190dbb58d4af37cb4ac534b
Zuoming Shi <accountistaken@gmail.com>
2012-11-16 03:04:04 -0700

    Furthur Networking
    Coherent notification system between GUI and logic using Observer,
    ActionListeners, and Threading. Factoring out network from game logic +
    comments still to be complete.

---
+++ TowerDefense/src/network/Liason.java
@@ -0,0 +1,61 @@
+package network;
+/**
+ * Multi-Client Chat Program
+ * Written by Zuoming Shi and Victor Nguyen, for CS335 Fall 2012.
+ **/
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Vector;
+
+class Liason extends Thread {
+	private Socket socketFromServer;
+	private ObjectInputStream readFromClient;
+	private Server server;
+	/**
+	 * Construct an object that will run in it's own Thread so this object can
+	 * communicate with that one connection
+	 */
+	public Liason(Socket socketFromServer,
+			Vector<String> sharedCollectionReference, Server S) {
+		this.socketFromServer = socketFromServer;
+		server = S;
+	}
+	/**
+	 * The void run() method in class Thread must be overridden in every class
+	 * that extends Thread. The Server created a new instance of this class and
+	 * sends it a start method, which is in Thread. The start method creates a
+	 * new Thread which then calls the following run method where our domain
+	 * specific logic goes.
+	 */
+	@Override
+	public void run() {
+		// Open the input and output streams so the
+		// client can interact with the collection
+		try {
+			readFromClient = new ObjectInputStream(socketFromServer.getInputStream());
+		} catch (IOException e) {
+			System.out.println("Exception thrown while obtaining input & output streams");
+			e.printStackTrace();
+			return;
+		}
+
+		String messageFromClient = null;
+		boolean stayConnected = true;
+		while (stayConnected) {
+			try {
+				messageFromClient = (String) readFromClient.readObject();
+			} catch (IOException e1) {
+				//Catch the closed connection exception to remove outputStream and Liason related to the client in the server.
+				server.removeLiason(getId());
+				return;
+			} catch (ClassNotFoundException e1) {
+				System.out.println("Error in Liason.run when trying to read from client");
+				e1.printStackTrace();
+			}
+			server.updateMessage(messageFromClient);
+
+		}
+	}
+}
---
+++ TowerDefense/src/network/Server.java
@@ -0,0 +1,102 @@
+package network;
+/**
+ * Multi-Client Chat Program
+ * Written by Zuoming Shi and Victor Nguyen, for CS335 Fall 2012.
+ **/
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Vector;
+
+/**
+ * This server is a thread that accepts requests to connect from clients. Each
+ * request cause a new Thread to handle the client, or in other words there is
+ * one thread per client on the server side.
+ * 
+ * Each client running in it's own thread shares the same database which is why
+ * the collection class has synchronized methods to allow one request to
+ * complete before another thread can jump into the middle. Use Vector<E>
+ */
+public class Server extends Thread {
+
+	// change these constants if needed
+	public static final int PORT_NUMBER = 4009;
+	public static final String HOST_NAME = "localhost";
+	private ServerSocket myServerSocket; // client request source
+	private Vector<String> connectedClients;
+	private ArrayList<ObjectOutputStream> listOutputStreams = new ArrayList<ObjectOutputStream>();
+	private ArrayList<Liason> listLiasons = new ArrayList<Liason>();
+	public static void main(String args[]) {
+		Server server = new Server(PORT_NUMBER);
+		// always call the start() method on a Thread. Don't call the run method.
+		server.start();
+	}
+
+	public Server(int port) {
+		connectedClients = new Vector<String>();
+		try {
+			myServerSocket = new ServerSocket(PORT_NUMBER);
+		} catch (IOException e) {
+			System.out.println("In Server.Server(),the constructor");
+		}
+	}
+
+	// This run() method is now in a separate Thread and will listen
+	// for connections at PORT_NUMBER at the machine in which it is running
+	@Override
+	public void run() {
+		try {
+			while (true) {
+				// accept blocks until request comes over socket
+				Socket intoServer = myServerSocket.accept();
+				
+				// For every new connection, start a new Thread that will
+				// communicate with that client. Each one will have access
+				// to the common collection of all users who are connected.
+				Thread t = new Liason(intoServer, connectedClients, this);//, idCount);
+				//idCount++;
+
+				if(intoServer == null) {continue;}
+				else {
+					if(intoServer.getOutputStream() != null)
+					listOutputStreams.add(new ObjectOutputStream(intoServer.getOutputStream()));
+					listLiasons.add((Liason)t);
+				// always call the start() method on a Thread. Don't call the run method.
+				t.start();
+				}
+			}
+		} catch (IOException e) {
+			System.out.println("In Server.run");
+			e.printStackTrace();
+		}
+	}
+	
+	//Sends message to via all avalible outputStreams to Clients.
+	public void updateMessage(String message){
+		ObjectOutputStream current;
+		for (int i = 0; i < listOutputStreams.size(); i++) {
+			try {
+				listOutputStreams.get(i).writeObject(message);				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	//Removes the Liason and the OutputStream connected to a Client upon the Client exiting.
+	public void removeLiason(long id){
+		System.out.println(id+":PassedID");
+		int i=0;
+		while(listLiasons.get(i).getId()!=id){
+			System.out.println(id+":LiasonID");
+			i++;
+			if(i==listLiasons.size()){return;}
+		}
+		listLiasons.remove(i);
+		listOutputStreams.remove(i);
+		
+	}
+}

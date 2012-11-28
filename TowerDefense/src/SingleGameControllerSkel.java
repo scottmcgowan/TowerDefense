5488625478c901d2646d1eef1c5ca49cbfc0ecff
Victor Nguyen <victornguyen@email.arizona.edu>
2012-11-27 21:19:34 -0700

    Single Player Panel

---
+++ TowerDefense/src/SingleGameControllerSkel.java
@@ -0,0 +1,108 @@
+
+import javax.swing.JFrame;
+
+import GUI.GameCanvas;
+import GUI.MultiPlayerShopPanel;
+import GUI.SinglePlayerShopPanel;
+
+public class SingleGameControllerSkel {
+
+	// main game class
+	static final int UPDATE_RATE = 60; // number of game update per second
+	static final long UPDATE_PERIOD = 1000000000L / UPDATE_RATE; // nanoseconds
+
+	// State of the game
+	boolean gameOver = false;
+	boolean gamePaused = false;
+	public int frameCounter = 0;
+	public int secondCounter = 0;
+	//public GameCanvas gameCanvas;
+	private GameCanvas canvas;
+	private SinglePlayerShopPanel shop;
+
+	public static void main(String[] args) {
+		SingleGameControllerSkel game = new SingleGameControllerSkel();
+		}
+
+	public static void wait (int n){
+        long t0,t1;
+        t0=System.currentTimeMillis();
+        do{
+            t1=System.currentTimeMillis();
+        }
+        while (t1-t0<1000);
+	}
+	
+	public SingleGameControllerSkel() {
+		
+		JFrame gui = new JFrame();
+		gui.setLayout(null);
+		shop = new SinglePlayerShopPanel();
+		canvas = new GameCanvas();
+		shop.connectToMap(canvas);
+		canvas.setSize(canvas.PANEL_WIDTH, canvas.PANEL_HEIGHT);
+		shop.setSize(shop.PANEL_WIDTH, shop.PANEL_HEIGHT);
+		canvas.setLocation(70, 20);
+		shop.setLocation(20, canvas.PANEL_HEIGHT+40);
+		gui.setTitle("Game");
+		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gui.setSize(shop.PANEL_WIDTH+50, canvas.PANEL_HEIGHT+shop.PANEL_HEIGHT+90);
+		gui.setVisible(true);
+		gui.add(shop);
+		gui.add(canvas);
+		
+		gameStart();
+	}
+	
+	public void gameStart() {
+		// gui = new GameGUI();
+		// Create a new thread
+		Thread gameThread = new Thread() {
+			// Override run() to provide the running behavior of this thread.
+			@Override
+			public void run() {
+				gameLoop();
+			}
+		};
+		// Start the thread. start() calls run(), which in turn calls
+		// gameLoop().
+		gameThread.start();
+	}
+
+	public void gameUpdate() {
+		//get some gameLogic in here!
+	}
+
+	// Run the game loop here.
+	private void gameLoop() {
+		// Regenerate the game objects for a new game
+		// ......
+
+		// Game loop
+		long beginTime, timeTaken, timeLeft;
+		while (true) {
+			beginTime = System.nanoTime();
+			if (gameOver)
+				break; // break the loop to finish the current play
+			if (!gamePaused) {
+				// Update the state and position of all the game objects,
+				// detect collisions and provide responses.
+				gameUpdate();
+			}
+			// Refresh the display
+
+			// Delay timer to provide the necessary delay to meet the target
+			// rate
+			timeTaken = System.nanoTime() - beginTime;
+			timeLeft = (UPDATE_PERIOD - timeTaken) / 1000000; // in milliseconds
+			if (timeLeft < 10)
+				timeLeft = 10; // set a minimum
+			try {
+				// Provides the necessary delay and also yields control so that
+				// other thread can do work.
+				Thread.sleep(timeLeft);
+			} catch (InterruptedException ex) {
+			}
+		}
+	}
+}

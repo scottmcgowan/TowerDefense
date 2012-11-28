99dcbaf0f68ace68162469babb2605d3426c4257
Fei <dfei@email.arizona.edu>
2012-11-28 13:24:36 -0700

    Commit testing for GameController

---
+++ TowerDefense/src/GUI/GameController.java
@@ -0,0 +1,17 @@
+package GUI;
+
+import java.util.List;
+
+import javax.swing.Timer;
+
+import model.Player;
+
+public class GameController {
+
+	private Player thisPlayer;
+	private Player otherPlayer;
+	private List orders;
+	private List spawnQueue;
+	private Timer timer;
+
+}
